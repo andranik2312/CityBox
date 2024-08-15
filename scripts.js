e1bccf968454e334501f97653be4413f1561271c
Andranik <andranik23121989@gmail.com>
2024-08-16 01:59:20 +0300

    Додав функціонал для кнопки та її стилізацію

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+// Додаємо подію при завантаженні контенту сторінки
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Сторінка повністю завантажена і готова до роботи.");
+    // Знаходимо кнопку за її id
+    const greetButton = document.getElementById('greetButton');
+    
+    // Додаємо обробник подій на натискання кнопки
+    greetButton.addEventListener('click', function() {
+        alert('Привіт, дякуємо за натискання кнопки!'); // Виводимо повідомлення при натисканні кнопки
+    });
 });
