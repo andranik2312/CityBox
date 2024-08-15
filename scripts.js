67d154c6c6be5d9941a80872e1ad3bfbcfb7cf63
Andranik <andranik23121989@gmail.com>
2024-08-16 02:48:21 +0300

    Додав новий функціонал для кнопок та стилі для товарів

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 // Додаємо подію при завантаженні контенту сторінки
 document.addEventListener("DOMContentLoaded", function() {
-    // Знаходимо кнопку за її id
+    // Знаходимо кнопку за її id "greetButton"
     const greetButton = document.getElementById('greetButton');
     
-    // Додаємо обробник подій на натискання кнопки
+    // Додаємо обробник подій на натискання кнопки "greetButton"
     greetButton.addEventListener('click', function() {
         alert('Привіт, дякуємо за натискання кнопки!'); // Виводимо повідомлення при натисканні кнопки
+    });
+
+    // Знаходимо кнопку за її id "moreInfoButton"
+    const moreInfoButton = document.getElementById('moreInfoButton');
+    
+    // Додаємо обробник подій на натискання кнопки "moreInfoButton"
+    moreInfoButton.addEventListener('click', function() {
+        alert('Більше інформації про нас буде додано скоро!'); // Виводимо повідомлення при натисканні кнопки
     });
 });
